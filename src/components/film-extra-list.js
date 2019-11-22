deb1f1bc17cdf6c2f1382b83213cb6fbfcacfd81
Anastasiya <v.anastasiya26@mail.ru>
2019-11-23 01:01:35 +0700

    задание 5

---
+++ src/components/film-extra-list.js
@@ -0,0 +1,9 @@
+const createFilmExtraList = text => {
+  return `<section class="films-list--extra">
+      <h2 class="films-list__title">${text}</h2>
+
+      <div class="films-list__container"></div>
+    </section>`;
+};
+
+export { createFilmExtraList };
