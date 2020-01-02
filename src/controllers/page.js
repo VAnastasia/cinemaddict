6d72a91b546ee21a95dc225fc15d95c18181be90
Anastasiya <v.anastasiya26@mail.ru>
2020-01-02 23:46:57 +0700

    задание 15

--- src/controllers/page.js
+++ src/controllers/page.js
@@ -6,8 +6,10 @@ import FilmPopupComponent from "../components/film-details";
 import ShowMoreComponent from "../components/show-more";
 import FilmExtraListComponent from "../components/film-extra-list";
 import NoFilmsComponent from "../components/no-films";
+import FilterComponent from "../components/filter";
+import FilmListComponent from "../components/film-list";
 import {render, Position} from "../utils";
-import {filmsRated, filmsCommented} from "../data";
+import {films, filmsRated, filmsCommented} from "../data";
 
 const siteMainElement = document.querySelector(`.main`);
 
@@ -54,26 +56,66 @@ export default class PageController {
   constructor(container) {
     this._container = container;
 
+    this._films = films.slice();
     this._showMoreComponent = new ShowMoreComponent();
     this._noFilmsComponent = new NoFilmsComponent();
+    this._filterComponent = new FilterComponent();
+    this._filmListComponent = new FilmListComponent();
   }
 
   render(filmsData) {
     const container = this._container;
-
-    const filmsContainer = document.querySelector(`.films`);
-    const filmsList = document.querySelector(`.films-list`);
+    let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
 
     if (filmsData.length === 0) {
-      render(siteMainElement, this._noFilmsComponent.getElement(), Position.BEFOREEND);
+      render(container, this._noFilmsComponent.getElement(), Position.BEFOREEND);
     } else {
-      filmsData.slice(0, SHOWING_FILMS_COUNT_ON_START).forEach((film) => {
-        renderFilm(film, container);
+      render(container, this._filterComponent.getElement(), Position.BEFOREEND);
+      render(container, this._filmListComponent.getElement(), Position.BEFOREEND);
+
+      const filmsList = document.querySelector(`.films-list`);
+      const filmListContainer = document.querySelector(`.films-list__container`);
+      const filterButtons = this._filterComponent.getElement().querySelectorAll(`.sort__button`);
+      const activeFilter = (filter) => {
+        Array.from(filterButtons).filter((button) => {
+          return button.dataset.sortType === filter;
+        })[0].classList.add(`sort__button--active`);
+      }
+
+      this._filterComponent.setSortTypeChangeHandler((sortType) => {
+        filterButtons.forEach((button) => {
+          button.classList.remove(`sort__button--active`);
+        });
+        switch (sortType) {
+          case `date`:
+            filmsData = this._films.slice().sort((a, b) => new Date(a.year) - new Date(b.year));
+            activeFilter(`date`);
+            break;
+          case `rating`:
+            filmsData = this._films.slice().sort((a, b) => b.rating - a.rating);
+            activeFilter(`rating`);
+            break;
+          case `default`:
+            filmsData = this._films.slice();
+            activeFilter(`default`);
+            break;
+        }
+
+        filmListContainer.innerHTML = null;
+        filmsData.slice(0, showingFilmsCount).forEach((film) => {
+          renderFilm(film, filmListContainer);
+        });
+      });
+
+      filmsData.slice(0, showingFilmsCount).forEach((film) => {
+        renderFilm(film, filmListContainer);
       });
 
       if (filmsData.length > SHOWING_FILMS_COUNT_ON_START) {
         render(filmsList, this._showMoreComponent.getElement(), Position.BEFOREEND);
       }
+
+      const filmsContainer = document.querySelector(`.films`);
 
       render(filmsContainer, new FilmExtraListComponent(`Top rated`).getElement(), Position.BEFOREEND);
       render(filmsContainer, new FilmExtraListComponent(`Most commented`).getElement(), Position.BEFOREEND);
@@ -91,17 +133,18 @@ export default class PageController {
       });
     }
 
-    let showingTasksCount = SHOWING_FILMS_COUNT_ON_START;
     this._showMoreComponent.setClickHandler(() => {
-      const prevTasksCount = showingTasksCount;
-      showingTasksCount = showingTasksCount + SHOWING_FILMS_COUNT_BY_BUTTON;
 
-      filmsData.slice(prevTasksCount, showingTasksCount)
+      const prevTasksCount = showingFilmsCount;
+      showingFilmsCount = showingFilmsCount + SHOWING_FILMS_COUNT_BY_BUTTON;
+      const filmListContainer = document.querySelector(`.films-list__container`);
+
+      filmsData.slice(prevTasksCount, showingFilmsCount)
         .forEach((film) => {
-          renderFilm(film, container);
+          renderFilm(film, filmListContainer);
         });
 
-      if (showingTasksCount >= filmsData.length) {
+      if (showingFilmsCount >= filmsData.length) {
         this._showMoreComponent.getElement().remove();
       }
     });
--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-// import FilmComponent from "./components/film";
-// import FilmPopupComponent from "./components/film-details";
 import MenuComponent from "./components/menu";
-import FilterComponent from "./components/filter";
+// import FilterComponent from "./components/filter";
 import ProfileComponent from "./components/profile";
-import FilmListComponent from "./components/film-list";
+// import FilmListComponent from "./components/film-list";
 import FooterStatisticComponent from "./components/footer-statistic";
 import PageController from "./controllers/page";
 
@@ -17,9 +15,8 @@ const siteFooterElement = document.querySelector(`footer`);
 
 render(siteHeaderElement, new ProfileComponent().getElement(), Position.BEFOREEND);
 render(siteMainElement, new MenuComponent().getElement(), Position.BEFOREEND);
-render(siteMainElement, new FilterComponent().getElement(), Position.BEFOREEND);
-render(siteMainElement, new FilmListComponent().getElement(), Position.BEFOREEND);
+// render(siteMainElement, new FilterComponent().getElement(), Position.BEFOREEND);
+// render(siteMainElement, new FilmListComponent().getElement(), Position.BEFOREEND);
 render(siteFooterElement, new FooterStatisticComponent(films.length).getElement(), Position.BEFOREEND);
-const filmListContainer = document.querySelector(`.films-list__container`);
 
-new PageController(filmListContainer).render(films);
+new PageController(siteMainElement).render(films);
