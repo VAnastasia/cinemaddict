7139044237c5a98225860ab0c57946ca5d775a04
Anastasiya <v.anastasiya26@mail.ru>
2020-01-26 15:53:02 +0700

    statistics

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -3,6 +3,7 @@ import {defineUserRating} from "./profile";
 
 import Chart from "chart.js";
 import ChartDataLabels from "chartjs-plugin-datalabels";
+
 import moment from "moment";
 
 const compareDates = (compareDate) => {
@@ -47,13 +48,13 @@ const filtredFilms = (films) => {
   };
 };
 
-const createStatisticsTemplate = (watchedFilmsAmount, totalDuration, topGenre, filmWatchedAmount, activeFilter) => {
+const createStatisticsTemplate = (watchedFilmsAmount, totalDuration, topGenre, activeFilter) => {
   return (
     `<section class="statistic">
        <p class="statistic__rank">
         Your rank
         <img class="statistic__img" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-        <span class="statistic__rank-label">${defineUserRating(filmWatchedAmount)}</span>
+        <span class="statistic__rank-label">${defineUserRating(watchedFilmsAmount)}</span>
       </p>
 
       <form action="https://echo.htmlacademy.ru/" method="get" class="statistic__filters">
@@ -81,7 +82,7 @@ const createStatisticsTemplate = (watchedFilmsAmount, totalDuration, topGenre, f
       </ul>
 
       <div class="statistic__chart-wrap">
-        <canvas class="statistic__chart" width="1000"></canvas>
+        <canvas class="statistic__chart" width="1000" height="400"></canvas>
       </div>
 
     </section>`
@@ -94,9 +95,11 @@ export default class StatisticsComponent extends AbstractSmartComponent {
     this._moviesModel = moviesModel;
     this._films = this._moviesModel.getFilmsAll();
     this._filmsWatched = this._films.filter((film) => film.watched);
-    this._genres = new Set([]);
-    this._genresWatched = {};
+    // this._genres = new Set([]);
+    // this._genresWatched = {};
     this._activeFilter = `all-time`;
+    this._chart = null;
+    this._renderCharts();
   }
 
   recoveryListeners() {
@@ -104,40 +107,47 @@ export default class StatisticsComponent extends AbstractSmartComponent {
   }
 
   getWatchedFilms() {
-    return this._filmsWatched.length;
+    const watchedFilms = filtredFilms(this._filmsWatched)[this._activeFilter];
+
+    return watchedFilms.length;
   }
 
   getTotalDuration() {
-    const totalTime = this._filmsWatched
+    const watchedFilms = filtredFilms(this._filmsWatched)[this._activeFilter];
+
+    const totalTime = watchedFilms
       .map((film) => film.runtime)
       .reduce((total, runtime) => total + runtime);
     return `${Math.floor(totalTime / 60)}<span class="statistic__item-description">h</span> ${totalTime % 60}<span class="statistic__item-description">m</span>`;
   }
 
-  getWatchedGenresAmount() {
-    this._filmsWatched.forEach((film) => {
+  getWatchedGenresAmount(filmsData) {
+    let genresSet = new Set([]);
+    const genresWatched = {};
+
+    filmsData.forEach((film) => {
       film.genres.forEach((genre) => {
-        this._genres.add(genre);
+        genresSet.add(genre);
       });
     });
 
-    const genres = Array.from(this._genres);
+    const genres = Array.from(genresSet);
     genres.forEach((genre) => {
-      this._genresWatched[genre] = 0;
+      genresWatched[genre] = 0;
     });
 
-    this._filmsWatched.forEach((film) => {
+    filmsData.forEach((film) => {
       film.genres.forEach((genre) => {
-        this._genresWatched[genre] = this._genresWatched[genre] + 1;
+        genresWatched[genre] = genresWatched[genre] + 1;
       });
 
     });
 
-    const genresStatistics = Object.keys(this._genresWatched)
+    const genresStatistics = Object.keys(genresWatched)
       .map((genre) => {
         return {
           name: genre,
-          amount: this._genresWatched[genre]
+          amount: genresWatched[genre]
         };
       });
 
@@ -145,22 +155,29 @@ export default class StatisticsComponent extends AbstractSmartComponent {
   }
 
   getTopGenre() {
-    const genres = this.getWatchedGenresAmount();
+    const watchedFilms = filtredFilms(this._filmsWatched)[this._activeFilter];
+
+    const genres = this.getWatchedGenresAmount(watchedFilms);
     return genres[0].name;
   }
 
   getTemplate() {
-    return createStatisticsTemplate(this.getWatchedFilms(), this.getTotalDuration(), this.getTopGenre(), this.getWatchedFilms(), this._activeFilter);
+    const watchedFilmsAmount = this.getWatchedFilms();
+    const totalDuration = this.getTotalDuration();
+    const topGenre = this.getTopGenre();
+    return createStatisticsTemplate(watchedFilmsAmount, totalDuration, topGenre, this._activeFilter);
   }
 
   setOnFilterClickHandler() {
     this.getElement().querySelector(`.statistic__filters`)
       .addEventListener(`click`, (evt) => {
+        console.log(`change`);
+
         if (evt.target.tagName === `INPUT`) {
           if (this._activeFilter !== evt.target.value) {
             this._activeFilter = evt.target.value;
             this.rerender();
-            this.renderChart();
+            this._renderCharts();
           }
         }
       });
@@ -181,13 +198,13 @@ export default class StatisticsComponent extends AbstractSmartComponent {
 
     // this.setActiveFilter(this._activeFilter);
 
-    // this._renderCharts();
+    this._renderCharts();
   }
 
   update(newFilmsData) {
     this._films = newFilmsData;
     this.rerender();
-    // this._renderCharts();
+    this._renderCharts();
   }
 
   _renderCharts() {
@@ -197,6 +214,89 @@ export default class StatisticsComponent extends AbstractSmartComponent {
     }
 
     const watchedFilms = filtredFilms(this._filmsWatched)[this._activeFilter];
-    this._chart = this._createChart(watchedFilms);
+    if (watchedFilms.length > 0) {
+      this._chart = this._createChart(watchedFilms);
+    }
+  }
+
+  _createChart(films) {
+    const genres = this.getWatchedGenresAmount(films);
+
+    const canvas = this.getElement().querySelector(`.statistic__chart`);
+    const ctx = canvas.getContext(`2d`);
+
+    this._chart = new Chart(ctx, {
+      type: `horizontalBar`,
+      data: {
+        plagins: [ChartDataLabels],
+        labels: genres.map((genre) => genre.name),
+        datasets: [
+          {
+            data: genres.map((genre) => genre.amount),
+            backgroundColor: `#ffe800`,
+            strokeColor: `#ffe800`,
+            borderWidth: 1,
+            datalabels: {
+              anchor: `start`,
+              align: `start`,
+              offset: 50,
+              barThickness: 20,
+              categoryPercentage: 1,
+
+              // color: `#ffffff`,
+              // font: {
+              //   size: 16
+              // },
+              formatter: (value, context) =>
+                `${context.chart.data.labels[context.dataIndex]}             ${value}`
+            }
+          }
+        ]
+      },
+      options: {
+        responsiveAnimationDuration: 400,
+        maintainAspectRatio: false,
+        responsive: true,
+        plugins: {
+          datalabels: {
+            font: {
+              size: 16
+            },
+            color: `#ffffff`
+          }
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: false
+        },
+        layout: {
+          padding: {
+            left: 200
+          }
+        },
+        scales: {
+          xAxes: [
+            {
+              display: false,
+              ticks: {
+                stepSize: 1,
+                beginAtZero: true
+              }
+            }
+          ],
+          yAxes: [
+            {
+              // barThickness: 20,
+              display: false,
+              // barPercentage: 0.5,
+              // categoryPercentage: 1
+            }
+          ]
+        }
+      }
+    });
+    return this._chart;
   }
 }
